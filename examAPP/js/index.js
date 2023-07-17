05bc3addf8f9618fc830bb7a44ca387eb497e8ab
liujy20 <1365476638@qq.com>
2023-07-17 15:24:30 +0800

    首页渲染

--- examAPP/js/index.js
+++ examAPP/js/index.js
@@ -1,19 +1,54 @@
-// TODU添加渲染
+let data = {};
 
+main();
 
-$('nav .item').click(function(){
-  location.href='/html/testList.html'
-})
+async function main() {
+  await render();
+  addClick();
+}
 
-$("header .menu").click(function(){
-  let ele=$('.sub-m')
-  ele.css('transform','scale(1)')
-  // $('.mask').css('display','block')
-  $('.mask').css('opacity','1')
-})
-$(".sub-m .close").click(function(){
-  let ele=$('.sub-m')
-  ele.css('transform','scale(0)')
-  $('.mask').css('opacity','0')
-  // $('.mask').css('display','none')
-})
+async function render() {
+  data = await getPromise(
+    "http://127.0.0.1:1234/students/indextypes",
+    "GET",
+    null
+  );
+  data = data.data;
+  console.log(data);
+  let arr = data.map((item) => {
+    return `<div class="item">
+    <img src="${item.icon}" alt="" class="item" />
+    <div class="tit">${item.type}</div>
+  </div>`;
+  });
+  $("nav").html(arr.join(""));
+}
+
+function addClick() {
+  // 考试跳转
+  $("nav .item").click(function () {
+    location.href = "/html/testList.html";
+  });
+  // 开启菜单
+  $("header .menu").click(function () {
+    let ele = $(".sub-m");
+    ele.css("transform", "scale(1)");
+    // $('.mask').css('display','block')
+    $(".mask").css("opacity", "1");
+  });
+  // 关闭菜单
+  $(".sub-m .close").click(function () {
+    let ele = $(".sub-m");
+    ele.css("transform", "scale(0)");
+    $(".mask").css("opacity", "0");
+    // $('.mask').css('display','none')
+  });
+  // 修改资料
+  $(".sub-m .item1").click(function () {
+    location.href = "/html/user.html";
+  });
+  // 退出登录
+  $(".sub-m .item2").click(function () {
+    location.href = "/html/login.html";
+  });
+}
