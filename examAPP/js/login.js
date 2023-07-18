d1ee38ba82cdda8c8f71cc327bbceb86dd6c2598
liujy20 <1365476638@qq.com>
2023-07-18 20:27:25 +0800

    校验

--- examAPP/js/login.js
+++ examAPP/js/login.js
@@ -11,19 +11,37 @@ $("main .con .pwd1 img").click(function () {
     img.prev().prop("type", "password");
   }
 });
-$('.register').click(function(){
-  location.href='/html/register.html'
-})
+$(".register").click(function () {
+  location.href = "/html/register.html";
+});
+const rePhone = /^1[3|4|5|7|8]\d{9}$/;
+const rePwd = /^[a-zA-Z][a-zA-Z0-9]{5,11}$/;
+let phone = $(".name.itemP input");
+let pwd = $(".pwd1.itemP input");
+// phone输入验证
+phone.on("input", () => {
+  if (rePhone.test(phone.val())) {
+    phone.css("border", "1px solid green");
+  } else {
+    phone.css("border", "1px solid red");
+  }
+});
+// pwd1输入验证
+pwd.on("input", () => {
+  if (rePwd.test(pwd.val())) {
+    pwd.css("border", "1px solid green");
+  } else {
+    pwd.css("border", "1px solid red");
+  }
+});
 
 // $(".login").click(async function () {
 //   location.href='/html/index.html'
 // })
 $(".login").click(async function () {
-  let name = $(".name.itemP input");
-  let pwd = $(".pwd1.itemP input");
   let res = await getPromise("http://127.0.0.1:1234/students/login", "POST", {
-    phone:name.val(),
-    password:pwd.val()
+    phone: phone.val(),
+    password: pwd.val(),
   });
   console.log(res);
   if (res.code == 1) {
