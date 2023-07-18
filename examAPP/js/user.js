02612cb6ffd263c9b09e402763cebb802f3e9a30
liujy20 <1365476638@qq.com>
2023-07-18 20:27:56 +0800

    修改信息

--- examAPP/js/user.js
+++ examAPP/js/user.js
@@ -4,6 +4,7 @@ main();
 function main() {
   render();
   addClick();
+  changeImg();
 }
 
 function render() {
@@ -38,7 +39,46 @@ function addClick() {
   });
   // 退出登录
   $(".sub-m .item4").click(function () {
-    localStorage.removeItem('token')
+    localStorage.removeItem("token");
     location.href = "/html/login.html";
+  });
+}
+
+function changeImg() {
+  $(".bg img").click(function () {
+    $(".bg input").click();
+  });
+  let formData = new FormData();
+  $(".bg input").on("change", function (event) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    let file = this.files[0];
+    if (!/^image\//.test(file.type)) {
+      alert("请上传图片");
+      return;
+    }
+    if (file.size > 90000) {
+      alert("图片过大");
+      return;
+    }
+
+    formData.append("icon", file);
+    $.ajax({
+      url: "http://127.0.0.1:1234/user/upload",
+      method: "POST",
+      data: formData,
+      success: function (data) {
+        alert(data.message);
+        console.log(data);
+        if (data.code == 200) {
+          $(".bg img").prop('src',`http://127.0.0.1:1234${data.data}`)
+        }//"http://127.0.0.1234/images/1689682689521-å¡.jpg"
+      },
+      //阻止jQuery传输数据时对数据进行转换处理
+      contentType: false,
+      processData: false,
+      cache: false,
+    });
+    // return false
   });
 }
