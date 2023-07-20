1fd5effe696f5f2247c5c89fe5e9d7b3b20390b5
liujy20 <1365476638@qq.com>
2023-07-20 19:27:53 +0800

    ASCII

---
+++ 99笔记/04ASCII.js
@@ -0,0 +1,10 @@
+let code1='a'
+let code2='A'
+console.log(code1.codePointAt(0));
+console.log(code2.codePointAt(0));
+
+console.log(code1.charCodeAt(0));
+console.log(code2.charCodeAt(0));
+
+console.log(String.fromCharCode(65))
+console.log(String.fromCodePoint(65))
