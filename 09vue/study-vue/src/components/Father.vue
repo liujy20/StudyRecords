e34c579387e7de94e2431d1cb5e761af48747e24
liujy20 <1365476638@qq.com>
2023-08-01 16:59:20 +0800

    插槽

---
+++ 09vue/study-vue/src/components/Father.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <son-vue>
+    <template v-slot:header>
+      <h1>header</h1>
+    </template>
+    <template v-slot:main>
+      <h2>main</h2>
+    </template>
+    <template v-slot:footer>
+      <h3>footer</h3>
+    </template>
+  </son-vue>
+  </div>
+</template>
+
+<script>
+import SonVue from "./Son.vue";
+export default {
+  components: {
+    SonVue,
+  },
+};
+</script>
+
+<style>
+*{
+  color: aquamarine;
+}
+</style>
---
+++ 09vue/study-vue/src/components/Son.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot name="header"></slot>
+    <slot name="main"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
