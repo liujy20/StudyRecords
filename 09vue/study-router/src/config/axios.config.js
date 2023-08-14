928a7a0f80a64ab17709a7f84d0aa91013f2c9f3
liujy20 <1365476638@qq.com>
2023-08-14 19:26:27 +0800

    update

--- 09vue/study-router/src/config/axios.config.js
+++ 09vue/study-router/src/config/axios.config.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 axios.defaults.baseURL='http://localhost:4001'
 
+// 请求过滤器
 axios.interceptors.request.use((config)=>{
   config.headers.Authorization=localStorage.getItem('token')
   return config
 })
 
+// 响应过滤器
 axios.interceptors.response.use((res)=>{
   return res
 })
