cdc154eaa161b4a9a767263172556e94ba1c175c
liujy20 <1365476638@qq.com>
2023-08-28 19:13:19 +0800

    init

--- WeChatProject/chabaidao/pages/list/list.js
+++ WeChatProject/chabaidao/pages/list/list.js
@@ -1,18 +1,81 @@
 // pages/list/list.js
+var QQMapWX = require('../../utils/qqmap-wx-jssdk.min');
+var qqmapsdk;
+qqmapsdk = new QQMapWX({
+  key: 'LZ5BZ-5JP34-KWJU4-K2WEC-U7ARO-BTBST'
+});
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    isClose: false,
+    userLocation: {
+      lat: 0,
+      lng: 0
+    },
+    stopList: [],
+    markers:[]
+  },
 
+  closeMap() {
+    this.setData({
+      isClose: !this.data.isClose
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    // console.log(options);
+    wx.getLocation({
+      type: 'gcj02',
+      success: (res) => {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        console.log('当前地址', res);
+        this.setData({
+          userLocation: {
+            lat: latitude,
+            lng: longitude+0.1
+          }
+        })
 
+        qqmapsdk.search({
+          keyword: '茶百道',
+          location: {
+            latitude:latitude,
+            longitude:longitude+0.1,
+          },
+          success: (res)=> {
+            console.log(res.data);
+            
+            let arr=res.data.map(item=>{
+              if(item._distance>1000){
+                let distance=item._distance/1000
+                item._distance=distance.toFixed(2)+'km'
+              }else{
+                item._distance=item._distance+'m'
+              }
+              return {
+                id:+item.id,
+                longitude:item.location.lng,
+                latitude:item.location.lat,
+                iconPath:'../../assets/icons/marker.png',
+                width:30,
+                height:30
+              }
+            })
+            this.setData({
+              stopList:res.data,
+              markers:arr
+            })
+          }
+        });
+      }
+    })
   },
 
   /**
@@ -54,7 +117,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    console.log('触底更新');
   },
 
   /**
--- WeChatProject/chabaidao/pages/order/order.js
+++ WeChatProject/chabaidao/pages/order/order.js
@@ -1,79 +1,114 @@
 // pages/order/order.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-      switchChecked:false,
-      type:[],
-      goods:[]
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    switchChecked: false,
+    type: [],
+    curIndex: 0,
+    curId:''
+  },
 
-    getGoods(){
-      wx.request({
-        url: 'http://124.70.54.24:3005/milkTeas/getAllCategory',
-        success:(res)=>{
-          console.log(res.data);
+  goMap(event){
+   console.log(event.currentTarget.dataset.name);
+   let name=event.currentTarget.dataset.name
+   wx.redirectTo({
+     url: `../list/list?name=${name}`,
+   })
+  },
+
+  getGoods() {
+    wx.request({
+      url: 'http://124.70.54.24:3005/milkTeas/getAllCategory',
+      success: (res) => {
+        console.log(res.data);
+        this.setData({
+          type: res.data.data
+        })
+      }
+    })
+  },
+
+  changeIndex(event) {
+    // console.log(event.currentTarget.dataset.index);
+    this.setData({
+      curIndex: event.currentTarget.dataset.index,
+      curId:'item-'+event.currentTarget.dataset.index
+    })
+  },
+  changeScroll(event) {
+    let scorllTop = event.detail.scrollTop
+    // console.log(scorllTop);
+    const query = wx.createSelectorQuery()
+    query.selectAll('.list-box').boundingClientRect((res)=> {
+      // console.log(res);
+      let allTop = 0;
+      for (let i in res) {
+        allTop += res[i].height
+        if (allTop > scorllTop) {
           this.setData({
-            type:res.data.data
+            curIndex: i
           })
+          break
         }
-      })
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-      this.getGoods()
-    },
+      }
+    }).exec()
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.getGoods()
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
 
-    },
+  },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
 
-    },
+  },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
 
-    }
+  }
 })
