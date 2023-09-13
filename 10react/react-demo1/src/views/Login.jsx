e387f4daff3c96ff57819efdcd65eff62caefd64
liujy20 <1365476638@qq.com>
2023-09-13 20:16:13 +0800

    login

---
+++ 10react/react-demo1/src/views/Login.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Button, Checkbox, Form, Input, ConfigProvider } from "antd";
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+export default function Login() {
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#52c41a",
+        },
+      }}
+    >
+      <div>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 500,
+            border: "1px solid #ccc",
+            padding: "50px 40px 20px",
+            margin: "100px auto",
+            borderRadius: "10px",
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </ConfigProvider>
+  );
+}
