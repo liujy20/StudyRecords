a5bfd5a297c225efb8a8d5b2755e6810d6fcad30
liujy20 <1365476638@qq.com>
2023-09-16 17:52:18 +0800

    选择器

--- 10react/react-demo1/src/components/AntdComp.jsx
+++ 10react/react-demo1/src/components/AntdComp.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { Button } from "antd";
+import React, { useState } from "react";
+import { Button, Select } from "antd";
 import style from "../assets/styles/AntdComp.module.less";
 export default function AntdComp() {
+  const [val,setVal]=useState('小五')
   return (
     <div>
       <h2 className={style.name}>AntdComp</h2>
@@ -10,6 +11,16 @@ export default function AntdComp() {
         <Button>按钮</Button>
         <Button type="dashed">按钮</Button>
       </div>
+      <Select
+        value={val}
+        style={{ width: "300px" }}
+        onChange={(event)=>setVal(event)}
+        options={[
+          { value: "小五", label: "小五" },
+          { value: "小六", label: "小六" },
+        ]}
+      ></Select>
+      <Button onClick={()=>setVal('')}>reset</Button>
     </div>
   );
 }
