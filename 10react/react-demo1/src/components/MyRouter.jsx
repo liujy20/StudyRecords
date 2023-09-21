5208809b0746d3296f01c33f6882463f3c2624eb
e5xwg <e5xwg@xwg.com>
2023-09-22 01:18:03 +0800

    动态路由

--- 10react/react-demo1/src/components/MyRouter.jsx
+++ 10react/react-demo1/src/components/MyRouter.jsx
@@ -1,15 +1,14 @@
 import { useRoutes } from "react-router-dom";
-import {defaultList,routerList} from "../routers";
+import { defaultList, routerList } from "../routers";
 import { useEffect, useState } from "react";
 export default function MyRouter() {
-  const [data,setData]=useState(defaultList)
-  const [userRole,setUserRole]=useState([])
+  const [data, setData] = useState(defaultList);
   useEffect(() => {
     init();
   }, []);
   const init = () => {
-    if(!localStorage.getItem('userInfo')){
-      return
+    if (!localStorage.getItem("userInfo")) {
+      return;
     }
     let info = JSON.parse(localStorage.getItem("userInfo")).role.menus;
 
@@ -18,33 +17,33 @@ export default function MyRouter() {
       console.log(arr[arr.length - 1]);
       return arr[arr.length - 1];
     });
-    setUserRole(role)
-    console.log('role',role);
-    let arr=roleRouter(routerList,[...data])
-    console.log('arr',arr);
+    console.log("role", role);
+    let arr = roleRouter(role,routerList, [...data]);
+    console.log("arr", arr);
+    setData(arr)
   };
-  const roleRouter=(arr,renderList=[])=>{
-    console.log(userRole);
+  const roleRouter = (role,arr, renderList = []) => {
     for (let item of arr) {
       if (!item.children) {
-        if (userRole.includes(item.name)) {
+        if (role.includes(item.name)) {
           renderList.push(item);
         }
-        continue
+        continue;
       } else {
         // console.log(item);
         // let children = roleMenu(item.children);
         // if (children) {
         //   renderList.push({ ...item, children });
         // }
-        if (userRole.includes(item.name)) {
+        if (role.includes(item.name)) {
           renderList.push(item);
         }
       }
     }
-    return renderList
-  }
+    return renderList;
+  };
   console.log(data);
-  let element = useRoutes(data);
+  // let element = useRoutes(data);
+  let element = useRoutes([...defaultList,...routerList]);
   return element;
 }
