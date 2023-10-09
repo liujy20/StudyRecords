db1d232bf1b1e01335f2ace0045ad762eb8ab9fe
liujy20 <1365476638@qq.com>
2023-10-09 14:07:48 +0800

    update

--- gitbms/demo.js
+++ gitbms/demo.js
@@ -1,7 +1,7 @@
 var WXBizDataCrypt = require('./tool/WXBizDataCrypt')
 
 var appId = 'wx1ad717865c973761'
-var sessionKey = '9e11c55b751340a6ffa867973bc0502b'
+var sessionKey = '9e11c55b751340a6ffa'//867973bc0502b'
 
 var encryptedData = "7xbWuqn9sdJ/sO2PQwYWNObAlzKwPsmMm3EeAOIl9qWSl7EUZ6DBUQ7QXbYhhpY4Fm/rrqgR+qP7rWgA1d6f9HEmtDTiXMlWdo9Skk1xBSq0zDAbkvqgQZbA9Nr9QNB/QsosIq8Tza7uh3nN+q6bBdqOE+5b82oX/uBr466oDa45EwmKKvY9HH4bTwYgsKXeWBtBIdijGMxG5CyTPFYsP0MGr3GTcNDbtm9gfzl1rjgLOJEaMpeOciBIMQrbUA/ZFtBS5U1JbqaXfUvl++IKcxYmvZqv7Nr3gaWrx/eGQARX5jE+J2qJjcVvLTaj+SzF3NDkztLXUAnIn1tDP9NfytkKzHxjO8HJNA/nGsKusQUWeTjTwh8i6QAjWfaIhNmg6Gss6XLWy9DAH5xfAQRs+Nefd4DZsMMTl31qyVTgo2s="
 var iv = "XVK8K3Rt+GX7RjP8zL4Srw=="
